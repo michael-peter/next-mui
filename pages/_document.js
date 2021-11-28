1adfd56739b67d508966355381dcf5350739060b
Michael Peter <kael@localhost.localdomain>
2021-11-28 15:03:19 +0100

    next with mui initial setup

---
+++ pages/_document.js
@@ -0,0 +1,36 @@
+import { ServerStyleSheets } from "@mui/styles"
+import Document, { Head, Html, Main, NextScript } from "next/document"
+import React from "react"
+
+export default class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="en">
+        <Head />
+
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
+
+MyDocument.getInitialProps = async (context) => {
+  const sheets = new ServerStyleSheets()
+  const originalRenderPage = context.renderPage
+  context.renderPage = () => {
+    return originalRenderPage({
+      enhanceApp: (App) => (props) => sheets.collect(<App {...props} />),
+    })
+  }
+  const initialProps = await Document.getInitialProps(context)
+  return {
+    ...initialProps,
+    styles: [
+      React.Children.toArray(initialProps.styles),
+      sheets.getStyleElement(),
+    ],
+  }
+}
